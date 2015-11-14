22041b3acead9c6cd95889220b8eb82e4b6e3003
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-11-13 21:06:12 -0300

    connect message handling.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- irc.erl
+++ irc.erl
@@ -34,30 +34,53 @@
 %% The server process will be registered by the name "ircd"
 
 -module(irc).
--export([start_server/0, server_loop/1, client_loop/1,connect/1]).
+-export([start_server/0, server_loop/1, client/1, client_loop/1,connect/1]).
 
 %% Change this to specify the node where the server runs.
 %% TODO: This can be set in a different way at runtime using a config file or by command line argument when converting this to an Application.
--define(SERVER_NODE, 'server@vagrant-unbuntu-trusty-64').
+-define(SERVER_NODE, 'server@vagrant-ubuntu-trusty-64').
 -define(SERVER_INSTANCE_NAME, irc_server).
 -define(CLIENT_INSTANCE_NAME, irc_client).
 
 %% @doc Start the server process and register it with known name.
 start_server() ->
-	error_logger:info_msg("Creating server process under name: ~s~n", [?SERVER_INSTANCE_NAME]),
+	error_logger:info_msg("Server > Creating server process under name: ~s~n", [?SERVER_INSTANCE_NAME]),
 	register(?SERVER_INSTANCE_NAME, spawn(irc, server_loop, [[]])).
 
 server_loop(User_List) ->
-	server_loop(User_List).
+	receive
+		{From, connect, Nickname} ->
+			error_logger:info_msg("Server > Received a connect message from ~s", [Nickname]),
+		       	Updated_User_List = server_handle_connect(User_List, Nickname)
+	end,
+	server_loop(Updated_User_List).
+
+server_handle_connect(User_List, Nickname) ->
+	[Nickname | User_List].
 
 connect(Nickname) ->
 	case whereis(?CLIENT_INSTANCE_NAME) of
 		undefined ->
-			error_logger:info_msg("Creating client process under name: ~s~n", [?CLIENT_INSTANCE_NAME]),
-		       	register(?CLIENT_INSTANCE_NAME, spawn(irc, client_loop, [Nickname]));
+			error_logger:info_msg("Client > Creating client process under name: ~s~n", [?CLIENT_INSTANCE_NAME]),
+		       	register(?CLIENT_INSTANCE_NAME, spawn(irc, client, [Nickname]));
 		_ -> 
-			error_logger:error_msg("Client process under name ~s already exists.~n", [?CLIENT_INSTANCE_NAME]),
+			error_logger:error_msg("Client > Client process under name ~s already exists.~n", [?CLIENT_INSTANCE_NAME]),
 		   	already_connected
+	end.
+
+client(Nickname) ->
+	error_logger:info_msg("Client > Sending connect command to server using Nickname ~s", [Nickname]),
+	{?SERVER_INSTANCE_NAME, ?SERVER_NODE} ! {self(), connect, Nickname},
+	await_result(),
+	client_loop(Nickname).
+
+await_result() ->
+	receive
+		{?SERVER_INSTANCE_NAME, stop, Why} ->
+			error_logger:error_msg("Client > Server says to stop. Exiting because of ~p~n", [Why]),
+			exit(normal);
+		{?SERVER_INSTANCE_NAME, What} ->
+			error_logger:info_msg("Client > Received ~p from server~n", [What])
 	end.
 
 client_loop(Nickname) ->
