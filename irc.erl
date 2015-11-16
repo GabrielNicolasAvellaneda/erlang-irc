8d05fe885b2c4f3dc9370ef5b3bb7fafe2393c76
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-11-16 18:53:18 -0300

    spec definitions, more unit tests, and more refactorings.

--- irc.erl
+++ irc.erl
@@ -37,7 +37,7 @@
 %% Separation of concerns: Client logic, Server handling, Server business rules, State model management.
 
 -module(irc).
--export([start_server/0, server_loop/1, client/1, client_loop/1,connect/1,list/0,join/1]).
+-export([start_server/0, server_loop/1, client/1, client_loop/1, connect/1, list/0, join/1, names/1]).
 
 -include_lib("eunit/include/eunit.hrl").
 
@@ -47,14 +47,23 @@
 -define(SERVER_INSTANCE_NAME, irc_server).
 -define(CLIENT_INSTANCE_NAME, irc_client).
 
--record(channel, {name, topic="", users = []}).
--record(server_state, {users = [], channels = []}).
+-record(user, {pid, name}).
+-record(channel, {name, users = [] :: [user()]}).
+-record(server_state, {users = [] :: [user()], channels = [] :: [channel()]}).
+
+-opaque server_state() :: #server_state{}.
+-opaque channel() :: #channel{}.
+-opaque user() :: #user{}.
+
+-export_type([server_state/0, channel/0, user/0]).
 
 %% @doc Start the server process and register it with known name.
 start_server() ->
-	error_logger:info_msg("Server > Creating server process under name: ~s~n", [?SERVER_INSTANCE_NAME]),
-	register(?SERVER_INSTANCE_NAME, spawn(irc, server_loop, [#server_state{}])).
+	Pid = spawn(irc, server_loop, [server_state_create()]),
+	register(?SERVER_INSTANCE_NAME, Pid),
+	error_logger:info_msg("Server > Created server process under name: ~p and pid: ~p~n", [?SERVER_INSTANCE_NAME, Pid]).
 
+-spec server_loop(server_state()) -> any().
 server_loop(ServerState) ->
 	receive
 		{From, connect, Nickname} ->
@@ -64,47 +73,77 @@ server_loop(ServerState) ->
 			error_logger:info_msg("Server > Received a list message~n"),
 			UpdatedServerState = server_handle_list(From, ServerState);
 		{From, join, ChannelName} ->
-			error_logger:info_msg("Server > Received a join(~s) message", [ChannelName]),
-			UpdatedServerState = server_handle_join(From, ServerState, ChannelName)
+			error_logger:info_msg("Server > Received a join(~s) message from(~p)", [ChannelName, From]),
+			UpdatedServerState = server_handle_join(From, ServerState, ChannelName);
+		{From, names, ChannelName} ->
+			error_logger:info_msg("Server > Received a names(~s) message", [ChannelName]),
+			UpdatedServerState = server_handle_names(From, ServerState, ChannelName)
 	end,
 	server_loop(UpdatedServerState).
-
+-spec server_tell(identifier(), any()) -> any().
 server_tell(To, Message) ->
 	To ! {?SERVER_INSTANCE_NAME, Message}.
 
 
 %% @doc Create a new server_state. Encapsulates server_state record creation.
-server_state_create() ->
-	#server_state{}.
+-spec server_state_create() -> server_state().
+server_state_create() -> #server_state{}.
 
+-spec server_state_create_user(any(), string()) -> user().
+server_state_create_user(Pid, Nickname) -> #user{pid=Pid, name=Nickname}.
+
+-spec user_get_name(user()) -> string().
+user_get_name(User) -> User#user.name.
+
+-spec server_state_create_channel(string()) -> channel().
 server_state_create_channel(ChannelName) ->
 	server_state_create_channel(ChannelName, []).
 
+-spec server_state_create_channel(string(), [user()]) -> channel().
 server_state_create_channel(ChannelName, Users) ->
 	#channel{name=ChannelName, users=Users}.
 
 %% @doc get users from server_state 
--spec server_state_get_users(#server_state{})->list(tuple()).
+-spec server_state_get_users(server_state()) -> [user()].
 server_state_get_users(#server_state{users = Users}) -> Users.
 
 %% @doc set users on server_state
+-spec server_state_set_users(server_state(), [user()]) -> server_state().
 server_state_set_users(ServerState, UpdatedUsers) -> ServerState#server_state{users = UpdatedUsers}.
 
 %% @doc add a user to the server_state
--spec server_state_add_user(#server_state{}, tuple()) -> #server_state{}.
+-spec server_state_add_user(server_state(), user()) -> server_state().
 server_state_add_user(ServerState, User) ->
 	UserList = server_state_get_users(ServerState),
 	UpdatedUsers = [User | UserList],
 	server_state_set_users(ServerState, UpdatedUsers).
 
 %% @doc check if a user exists
-server_state_has_user(ServerState, {_Sender, Nickname}) -> lists:keymember(Nickname, 2, server_state_get_users(ServerState)).
+-spec server_state_has_user(server_state(), user()) -> boolean().
+server_state_has_user(ServerState, User) -> 
+	Nickname = user_get_name(User),
+	lists:any(fun (U) -> user_get_name(U) =:= Nickname end, server_state_get_users(ServerState)).
+
+-spec find(fun( (_) -> boolean()), [any()]) -> any() | 'false'. 
+%% TODO: Optimize this to get the first match.
+find(Pred, List) ->
+	Matches = lists:filter(Pred, List),
+	if
+		length(Matches) > 0 -> hd(Matches);
+		true -> false
+	end.
 
 %% @doc get a user from the server_state record.
-server_state_get_user_by_sender(ServerState, Sender) -> lists:keyfind(Sender, 1, server_state_get_users(ServerState)). 
+-spec server_state_find_user_by_sender(any(), server_state()) -> user() | 'false'. 
+server_state_find_user_by_sender(Sender, ServerState) ->
+	io:format("Searching for Sender ~p in ~p", [Sender, ServerState]),
+	find(fun (X) -> X#user.pid == Sender end, server_state_get_users(ServerState)).
 
-%% TODO: Optimize this to get the first match.
-%% TODO: How to specify types that returns records
+-spec server_state_find_user_by_name(string(), server_state()) -> user() | 'false'.
+server_state_find_user_by_name(Nickname, ServerState) ->
+	find(fun (X) -> X#user.name =:= Nickname end, server_state_get_users(ServerState)).
+
+-spec channels_find_by_name(string(), [channel()]) -> 'false' | channel(). 
 channels_find_by_name(Name, Channels) ->
 	Result = lists:filter(fun (X) -> X#channel.name =:= Name end, Channels),
 	if
@@ -112,68 +151,97 @@ channels_find_by_name(Name, Channels) ->
 		true -> false
 	end.
 
+-spec channel_get_name(channel()) -> string(). 
 channel_get_name(Channel) -> Channel#channel.name.
 
 %% @doc get channels from server_state
+-spec server_state_get_channels(server_state()) -> [channel()].
 server_state_get_channels(#server_state{channels = Channels}) -> Channels.
 
 %% @doc set channels to server_state
+-spec server_state_set_channels(server_state(), [channel()]) -> server_state().
 server_state_set_channels(ServerState, UpdatedChannels) -> ServerState#server_state{channels = UpdatedChannels}.
 
+-spec server_state_add_channel(server_state(), channel()) -> server_state().
 server_state_add_channel(ServerState, Channel) ->
 	Channels = server_state_get_channels(ServerState),
 	UpdatedChannels = [Channel | Channels],
 	server_state_set_channels(ServerState, UpdatedChannels).
 
-channel_users(#channel{users=Users}) -> Users.
+-spec channel_get_users(channel()) -> [user()].
+channel_get_users(#channel{users=Users}) -> Users.
 
-channel_users(Channel = #channel{}, UpdatedUsers) -> Channel#channel{users=UpdatedUsers}.
+-spec channel_set_users(channel(), [user()]) -> channel().
+channel_set_users(Channel = #channel{}, UpdatedUsers) -> Channel#channel{users=UpdatedUsers}.
 
 %% @doc add user to server_state if the user is not already registered. This is business logic. 
 %% TODO: Get a better name, like may_connect_user?
+-spec may_add_user_to_server_state(server_state(), user()) -> {ok, server_state()} | {error, user_alredy_exists}.
 may_add_user_to_server_state(ServerState, User) -> 
 	case server_state_has_user(ServerState, User) of
-	      	true -> {error, user_already_exists};
-		false -> {ok, server_state_add_user(ServerState, User)}
+		false -> {ok, server_state_add_user(ServerState, User)};
+	      	true -> {error, user_already_exists}
 	end.
 
-server_state_channels_find_by_name(ServerState, ChannelName) ->
-	ok.
+-spec may_list_names_in_channel(string(), server_state()) -> [string()].
+may_list_names_in_channel(ChannelName, ServerState) ->
+	%% TODO: What happens if channel not found.
+	Channel = server_state_channels_find_by_name(ChannelName, ServerState),
+	Users = channel_get_users(Channel),
+	lists:map(fun (U) -> user_get_name(U) end, Users).
 
-server_state_channels_add(Channel) ->
-	ok.
+-spec server_state_channels_find_by_name(string(), server_state()) -> channel().
+server_state_channels_find_by_name(ChannelName, ServerState) ->
+	channels_find_by_name(ChannelName, server_state_get_channels(ServerState)).
+
+-spec server_state_channels_add(channel(), server_state()) -> server_state().
+server_state_channels_add(Channel, ServerState) ->
+	Channels = server_state_get_channels(ServerState),
+	UpdatedChannels = channels_add(Channel, Channels),
+	server_state_set_channels(ServerState, UpdatedChannels).
 
 %% @doc List the all the channel names in the server
+-spec server_list_channel_names(server_state()) -> [string()].
 server_list_channel_names(ServerState) ->
 	Channels = server_state_get_channels(ServerState),
 	lists:map(fun channel_get_name/1, Channels).
 
+-spec channels_remove(channel(), [channel()]) -> [channel()].
+channels_remove(Channel, Channels) -> Channels -- [Channel].
+
+-spec channels_add(channel(), [channel()]) -> [channel()].
+channels_add(Channel, Channels) -> [Channel | Channels ].
+
+%% TODO: Reverse parameters.
+-spec channel_add_user(channel(), user()) -> channel().
+channel_add_user(Channel, User) ->
+	UserList = channel_get_users(Channel),
+	UpdatedUsers = [User | UserList],
+	channel_set_users(Channel, UpdatedUsers).
+
 %% Separate logic into Server Message Handling, Data Structure(Model), Business Rules.
 
 %% TODO: Refactor this to be more readable.
+-spec add_channel_and_user_to_server_state(server_state(), string(), user()) -> server_state(). 
 add_channel_and_user_to_server_state(ServerState, ChannelName, User) ->
 	Channels = server_state_get_channels(ServerState),
-	Channel = channels_find_by_name(ChannelName, Channels),
-	if
-		Channel == false ->
-			NewChannel = #channel{name=ChannelName, users=[User]},
-			UpdatedChannels = [NewChannel | Channels],
-			server_state_set_channels(ServerState, UpdatedChannels);
-		true ->
-			%% TODO: Do a unit test of this.
-			OtherChannels = Channels -- [Channel],
-			UserList = channel_users(Channel),
-			UpdatedUsers = [User | UserList],
-			UpdatedChannel = channel_users(Channel, UpdatedUsers),
-			UpdatedChannels = [UpdatedChannel | OtherChannels],
+	case channels_find_by_name(ChannelName, Channels) of
+		false ->
+			io:format("User: ~p", [User]),
+			NewChannel = server_state_create_channel(ChannelName, [User]),
+			io:format("NewChannel: ~p", [NewChannel]),
+			server_state_add_channel(ServerState, NewChannel);
+		Channel ->
+			OtherChannels = channels_remove(Channel, Channels),
+			UpdatedChannel = channel_add_user(Channel, User),
+			UpdatedChannels = channels_add(UpdatedChannel, OtherChannels),
 			server_state_set_channels(ServerState, UpdatedChannels)
 	end.
 
 %% @doc Handle a connect message
 server_handle_connect(Sender, ServerState, Nickname) ->
-	%% TODO: Encapsulate user structure?
 	%% TODO: Create different function for handling ok and error situations to remove the case?
-	case may_add_user_to_server_state(ServerState, {Sender, Nickname}) of
+	case may_add_user_to_server_state(ServerState, server_state_create_user(Sender, Nickname)) of
 		{ok, UpdatedServerState} ->
 			server_tell(Sender, connected),
 			UpdatedServerState;
@@ -182,16 +250,22 @@ server_handle_connect(Sender, ServerState, Nickname) ->
 			ServerState
 	end.
 
-%% TODO: Just returns the list of Channels without users.
 server_handle_list(Sender, ServerState) ->
 	ChannelNames = server_list_channel_names(ServerState),
 	server_tell(Sender, {list_response, ChannelNames}),
 	ServerState.
 
-server_handle_join(Sender, ServerState = #server_state{channels=ChannelList}, ChannelName) ->
-	User = server_state_get_user_by_sender(ServerState, Sender),
+server_handle_names(Sender, ServerState, ChannelName) ->
+	NamesInChannel = may_list_names_in_channel(ChannelName, ServerState),	
+	server_tell(Sender, {names_response, NamesInChannel}),
+	ServerState.
+
+server_handle_join(Sender, ServerState= #server_state{}, ChannelName) ->
+	User = server_state_find_user_by_sender(Sender, ServerState),
+	io:format("~nState: ~p, User: ~p", [ServerState, User]),
 	UpdatedServerState = add_channel_and_user_to_server_state(ServerState, ChannelName, User), 
 	server_tell(Sender, {join_response, ok}),
+	io:format("~p", [UpdatedServerState]),
 	UpdatedServerState.
 
 connect(Nickname) ->
@@ -211,10 +285,23 @@ list() ->
 			error_logger:info_msg("Client > Received list of channels from server ~p~n", [ChannelList])
 	end.
 
+-spec names(string()) -> any().
+names(ChannelName) ->
+	{?SERVER_INSTANCE_NAME, ?SERVER_NODE} ! {self(), names, ChannelName},
+	receive
+		{?SERVER_INSTANCE_NAME, {names_response, Names}} ->
+			error_logger:info_msg("Client > Received list of names ~p for channel ~s~n", [Names, ChannelName])
+	end.
+
+send_if_connected(Message) ->
+	case whereis(?CLIENT_INSTANCE_NAME) of
+		undefined ->
+			not_logged_on;
+		_ -> ?CLIENT_INSTANCE_NAME ! Message
+	end.
+
 join(ChannelName) ->
-	error_logger:info_msg("Client > Sending join(~s) command to server", [ChannelName]),
-	{?SERVER_INSTANCE_NAME, ?SERVER_NODE} ! {self(), join, ChannelName},
-	await_result().
+	send_if_connected({join, ChannelName}).
 
 client(Nickname) ->
 	error_logger:info_msg("Client > Sending connect command to server using Nickname ~s", [Nickname]),
@@ -231,7 +318,14 @@ await_result() ->
 			error_logger:info_msg("Client > Received ~p from server~n", [What])
 	end.
 
+%% @doc client_loop forwards messages to the server
 client_loop(Nickname) ->
+	receive
+		{join, ChannelName} ->
+			error_logger:info_msg("Client > Sending join(~s) command to server", [ChannelName]),
+			{?SERVER_INSTANCE_NAME, ?SERVER_NODE} ! {self(), join, ChannelName},
+			await_result()
+	end,
 	client_loop(Nickname).
 
 %% Tests for Server state model manipulation.
@@ -247,15 +341,33 @@ should_get_empty_users_for_a_newly_created_server_state_test() ->
 should_set_users_to_server_state_test() ->
 	ServerState = server_state_create(), 
 	%% TODO: Define a User type and think about a value that can be used for testing in replacement for a User Pid (?maybe an atom?).
-	AllUsers = [{user_pid, 'Mike'}, {user_pid, 'Jonhhy'}],
+	AllUsers = [server_state_create_user(user_pid, "Mike"), server_state_create_user(user_pid, "Jonhhy")],
 	UpdatedServerState = server_state_set_users(ServerState, AllUsers),
 	?assert(server_state_get_users(UpdatedServerState) =:= AllUsers).
 
 should_add_user_to_server_state_test() ->
 	ServerState = server_state_create(),
-	NewUser = {user_pid, "Emma"},
+	NewUser = server_state_create_user(user_pid, "Emma"),
 	UpdatedServerState = server_state_add_user(ServerState, NewUser),
 	?assert(server_state_get_users(UpdatedServerState) =:= [NewUser]).
+
+should_check_for_existing_user_on_server_state_test() ->
+	ServerState = server_state_create(),
+	NewUser = server_state_create_user(user_pid, "Bryan"),
+	UpdatedServerState = server_state_add_user(ServerState, NewUser),
+	?assert(server_state_has_user(UpdatedServerState, NewUser) =:= true).
+
+should_find_user_by_nickname_test() ->
+	ServerState = server_state_create(),
+	User = server_state_create_user(user_pid, "John"),
+	UpdatedServerState = server_state_add_user(ServerState, User),
+	?assert(server_state_find_user_by_name("John", UpdatedServerState) =:= User).
+
+should_find_user_by_sender_test() ->
+	ServerState = server_state_create(),
+	User = server_state_create_user(unique_user_identifier, "Rita"),
+	UpdatedServerState = server_state_add_user(ServerState, User),
+	?assert(server_state_find_user_by_sender(unique_user_identifier, UpdatedServerState) =:= User).
 
 should_get_empty_channels_for_a_newly_created_server_state_test() ->
 	ServerState = server_state_create(),
@@ -274,23 +386,36 @@ should_add_channel_to_server_state_test() ->
 	UpdatedServerState = server_state_add_channel(ServerState, Channel),
 	?assert(server_state_get_channels(UpdatedServerState) =:= [Channel]).
 
+should_find_channel_by_name_test() ->
+	ServerState = server_state_create(),
+	Channel = server_state_create_channel("MyChannel"),
+	UpdatedServerState = server_state_add_channel(ServerState, Channel),
+	?assert(server_state_channels_find_by_name("MyChannel", UpdatedServerState) =:= Channel).
+
 %% Tests for Server business rules.
 should_only_add_user_if_user_does_not_already_exists_test() ->
-	User = {user_pid, 'Vincent'},
+	User = server_state_create_user(user_pid, "Vincent"),
 	ServerState = server_state_add_user(server_state_create(), User),
 	?assert({error, user_already_exists} =:= may_add_user_to_server_state(ServerState, User)),
 	?assert(length(server_state_get_users(ServerState)) =:= 1),
-	NewUser = {new_user_pid, 'Sandra'},
+	NewUser = server_state_create_user(new_user_pid, "Sandra"),
 	{ok, UpdatedServerState} = may_add_user_to_server_state(ServerState, NewUser),
 	?assert(length(server_state_get_users(UpdatedServerState)) =:= 2). 
 
-should_get_list_channels_names_test() ->
+should_get_list_of_channels_names_test() ->
 	ServerState = server_state_create(),
 	Channel1 = server_state_create_channel("Channel 1"),
 	Channel2 = server_state_create_channel("Channel 2"),	
-	%% TODO: Use a set for managing channels? So when comparing lists we don't need take in account the ordering of the items?
+	%% TODO: Use a set for managing channels? So when comparing lists we don't need to take in account the ordering of the items?
 	UpdatedServerState = server_state_add_channel(server_state_add_channel(ServerState, Channel1), Channel2),
 	?assert(server_list_channel_names(UpdatedServerState) =:= ["Channel 2", "Channel 1"]). 
+
+should_get_list_of_names_of_users_in_channel_test() ->
+	User = server_state_create_user(user_pid, "Gabriel"),
+	ChannelName = "Channel 1",
+	Channel1 = server_state_create_channel(ChannelName, [User]),
+	ServerState = server_state_add_channel(server_state_create(), Channel1),
+	?assert(may_list_names_in_channel(ChannelName, ServerState) =:= ["Gabriel"]).
 
 should_only_add_channel_if_channel_does_not_already_exists_test() ->
 	%% TODO: Implement this.
